da81aba62990ae442322bc15ca7d73350d6df090
Jack Kilby <jackiehit@163.com>
2019-10-01 09:31:45 +0800

    [StringHandler] Add getchar method to count specif chars in sentence.

--- StringHandler/main.c
+++ StringHandler/main.c
@@ -2,6 +2,9 @@
 
 //Count specific character num in a sentence
 #define SENTENCE    "Jack is working on this program.\n"
+const char example_sentence[] = "That is a \' sentence.\'\"";
+
+static void example_program_7_6_in_C_Primer_Plus(void);
 
 typedef enum {
     CHAR_COUNT_WITHOUT_SPECIFIC_CHAR,
@@ -29,9 +32,31 @@ long get_char_count_of_sentence(mode_t mod, const char sentence[], char specchar
     return count;
 }
 
+
 int main() {
-    long num = get_char_count_of_sentence(CHAR_COUNT_OF_SPECIFIC_CHAR, SENTENCE, 'k');
-    printf("the result is %d\n", num);
+    example_program_7_6_in_C_Primer_Plus();
     return 0;
 }
 
+static void example_program_7_6_in_C_Primer_Plus(void)
+{
+    unsigned int result = 0;
+    //To count characters in a sentence without "'" and """
+#if 0 //The method of using API
+    result = sizeof(example_sentence) - 1;
+    result -= get_char_count_of_sentence(CHAR_COUNT_OF_SPECIFIC_CHAR, example_sentence, '\'');
+    result -= get_char_count_of_sentence(CHAR_COUNT_OF_SPECIFIC_CHAR, example_sentence, '\"');
+#endif //
+#if 1 // The method of using getchar()
+    char ch;
+    printf("Now it's time for you to input your sentence! The '.' will make it end:\n");
+
+    while ( (ch = getchar()) != '.' ) {
+        if ('\'' != ch && '\"' != ch) {
+            result ++;
+        }
+    }
+#endif //
+
+    printf("The num of chars without ' and \" in sentence is %u\n", result);
+}
