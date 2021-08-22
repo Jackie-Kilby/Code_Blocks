20e2ed5fdf618a55303346dc81419cc8d906c3c8
Jack Kilby <jackiehit@163.com>
2021-08-22 16:33:02 +0800

    [MineSweeper] Framework of the game.

---
+++ LittleGames/MineSweeper/MineSweeper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //Init map
+    //Init field
+    printf("Hello, Mine Sweeper!\r\n");
+
+    while (1) {
+        //Ask user to input position to discover
+        //Check mine in field
+        //Refresh map
+    }
+    return 0;
+}
