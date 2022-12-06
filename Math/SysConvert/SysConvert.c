162b738f7f3e1665eba241a990c2913893deaea1
Jack Kilby <lonelyrainjk@163.com>
2022-12-06 12:50:40 +0800

    [SysConvert] Add a method (FOR_BITS) to convert decimal to binary, getting this method from bootk <嵌入式C编程实战>.

--- Math/SysConvert/SysConvert.c
+++ Math/SysConvert/SysConvert.c
@@ -3,6 +3,17 @@
  * ********************************************************/
 #include <stdio.h>
 
+
+#define PRINTF_BIT_EN	1
+
+#if PRINTF_BIT_EN
+#define printf_bit	printf	
+#else
+#define printf_bit
+#endif
+
+
+/*************************** Common system convert method *******************************/
 /***********************************************************
  * Function:    sys_convert_with_recursion 
  * Description: Change decimal to any system with recursion method.
@@ -16,16 +27,56 @@
 int res_rec = 0;
 int sys_convert_with_recursion(int toType, int dec)
 {
-    if(dec > 0)
-    {
+    if (dec > 0) {
         sys_convert_with_recursion(toType, dec/toType);
+		printf_bit("[%d]", dec%toType);
 		res_rec = res_rec*10 + dec%toType;
     }
 	return res_rec;
 }
 
+/*************************** Decimal to Binary *******************************/
+#define DECIMAL_TO_BINARY_METHOD_RECURSION	0
+#define DECIMAL_TO_BINARY_METHOD_FOR_BITS	1
+#define DECIMAL_TO_BINARY_METHOD			DECIMAL_TO_BINARY_METHOD_FOR_BITS
 /***********************************************************
- * Function:    sys_convert 
+ * Function:    sys_convert_dec_to_bin 
+ * Description: Change decimal to binary.
+ * Developer:   Jack Kilby
+ * First Breed: 2022-12-06
+ * Parametric:  int num: input decimal value.
+ * Return:      int : output value in Binary, using int value to show.
+ * ********************************************************/
+int sys_convert_dec_to_bin(int num)
+{
+	int bin = 0;
+
+	//Judge num bits
+	unsigned char bits = 31;
+	for (bits=31; bits>0; bits--) {
+		if ( num & (1<<bits) ) {
+			break;
+		}
+	}
+
+	for (int bitv = (1<<bits); bitv>0; bitv>>=1) {
+	/*		
+		if ( num & (1<<bits) ) {
+			bin = bin*10 + 1;
+		} else {
+			bin = bin*10;
+		}
+	*/
+		bin = bin*10 + !!(num & bitv);
+		printf_bit("[%d]", !!(num & bitv));	
+	}
+
+	return bin;
+}
+
+/*************************** Middle API *******************************/
+/***********************************************************
+ * Function:    sys_convert
  * Description: middle API, used to call different implementation accoring to fromType, toType.
  * Developer:   Jack Kilby
  * First Breed: 2022-12-04
@@ -42,7 +93,11 @@ int sys_convert(int fromType, int toType, int num)
 		case 10: {
 			switch (toType) {
 				case 2:
+#if (DECIMAL_TO_BINARY_METHOD == DECIMAL_TO_BINARY_METHOD_RECURSION)
 					res = sys_convert_with_recursion(2, num);
+#elif (DECIMAL_TO_BINARY_METHOD == DECIMAL_TO_BINARY_METHOD_FOR_BITS)
+					res = sys_convert_dec_to_bin(num);
+#endif
 					break;
 
 				default:
@@ -60,6 +115,7 @@ int sys_convert(int fromType, int toType, int num)
 	return res;
 }
 
+/*************************** Program Entry *******************************/
 /***********************************************************
  * Function:    main 
  * Description: Ask user to input system convert related value.
@@ -84,9 +140,9 @@ int main(void)
 
 	result = sys_convert(from_type, to_type, num);
 	if (result) {
-		printf("%d covert from System-%d to System-%d is: %d.\n", num, from_type, to_type, result);
+		printf("\n%d covert from System-%d to System-%d is: %d.\n", num, from_type, to_type, result);
 	} else {
-		printf("Sorry, System Convert from %d to %d is not supported now.", from_type, to_type);
+		printf("\nSorry, System Convert from %d to %d is not supported now.", from_type, to_type);
 	}
 	
     getchar();
